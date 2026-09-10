acc1c4c7640bc37221f24218dfdb58c86751e4e4
abukari-ops <a.bukari@alustudent.com>
2026-09-10 22:30:07 +0400

    added record  payment

--- payment.py
+++ payment.py
@@ -2,21 +2,41 @@ class Payment:
     """Represents a fee payment made by a student."""
 
     def __init__(self, payment_id, amount, date, student_id):
-# validate payment amount.
-        if amount <= 0:
-            raise ValueError("Payment amount must be greater than 0.")
-# check payment against outstanding balance.
-        if amount > outstanding_balance:
-            raise ValueError("Payment cannot be grater than the outstanding balance.")
-
         self.payment_id = payment_id
         self.amount = amount
         self.date = date
         self.student_id = student_id
+    def validate_amount(self):
+        """check whether the payment amount is valid."""
+        if self.amount <= 0:
+            raise ValueError("Payment amount must be greater than zero.")
+        return True
+    def check_outstanding_balance(self, outstanding_balance):
+        """Prevent a payment from exceeding the outstanding balance."""
+        if self.amount > outstanding_balance:
+            raise ValueError("Payment amount cannot be greater than the outstanding balance.")
+        return True
 
-payment1 = Payment("P001" , 5000, "2026-09-07" , "ST001")
+    
+def record_payment(payment_id, amount,date, student_id, outstanding_balance):
+        """Create and validate a new payment."""
+        payment = Payment(payment_id, amount, date, student_id)
+        payment.validate_amount()
+        payment.check_outstanding_balance(outstanding_balance)
+        return payment
 
-print(payment1.payment_id)
-print(payment1.amount)
-print(payment1.date)
-print(payment1.student_id)
+payment1 =Payment("P001", 5000, "2026-09-09", "ST001")
+payment1 = record_payment("P001", 5000, "2026-09-09", "ST001",10000)
+
+print("Payment recorded successfully!")
+print("Payment ID:", payment1.payment_id)
+print("Amount:", payment1.amount)
+print("Date:", payment1.date)
+print("Student ID:", payment1.student_id)
+payment3 = record_payment(
+    "P003",
+    15000,
+    "2026-09-10",
+    "ST001",
+    10000
+)
