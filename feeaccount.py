ea31e2ca73a35c0732194f9d1a55084bcf5a7b8c
Aishat <a.adedire@alustudent.com>
2026-09-11 23:12:16 +0400

    implemented payment addition and validation using add_payment

--- feeaccount.py
+++ feeaccount.py
@@ -15,3 +15,12 @@ class FeeAccount:
     def calculate_outstanding_balance(self):
         """Calculates the outstanding balance for the student."""
         return self.student.total_fees - self.calculate_total_paid()  #this subtracts the total amount paid from the total fees owed by the student.
+
+    def add_payment(self, payment):
+        """Adds a Payment object to the account history."""
+        if payment.student_id != self.student.student_id:       #checks that the payment belongs to the same student as the account
+            print("Error: Payment student ID does not match the account's student ID.")
+            return False
+
+        self.payments.append(payment)  #Adds the payment to the payments list.
+        return True
